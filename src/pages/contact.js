b2c4a16c1765c3b51963cce843c7585f8a3e04c5
hamdi lachaal <48292562+hklachaal@users.noreply.github.com>
2019-11-13 13:49:13 -0800

    contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,93 @@
 import React from "react"
 import Layout from "../components/layout"
+import { navigate } from "gatsby-link"
+import contactStyles from "./contact.module.scss"
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
 
 const ContactPage = () => {
+  const [state, setState] = React.useState({})
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+
   return (
-    <Layout pageTitle="Contact" pageDescription="Best way to contact Hamdi Lachaal">
-      <h1>Contact</h1>
-      <p>Send an email to hamdi.lachaal@gmail.com</p>
+    <Layout pageTitle="Contact" pageDescription="Best way to contact me.">
+      <div className={contactStyles.contactContainer}>
+        <h1>Contact</h1>
+        <form
+          name="contact"
+          method="POST"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={handleSubmit}
+        >
+          <input type="hidden" name="contact" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input name="bot-field" onChange={handleChange} />
+            </label>
+          </p>
+          <div className={contactStyles.formItem}>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Name"
+              required
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className={contactStyles.formItem}>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="email"
+              required
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className={contactStyles.formItem}>
+            <textarea
+              name="message"
+              id="message"
+              rows="5"
+              placeholder="message"
+              required
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className={contactStyles.formItem}>
+            <button type="submit">Send</button>
+          </div>
+        </form>
+      </div>
     </Layout>
   )
 }
