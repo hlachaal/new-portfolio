9cd9c3a74dc546c48e3cf707f6de85502ee7004d
hamdi lachaal <48292562+hklachaal@users.noreply.github.com>
2019-11-21 17:25:35 -0800

    added thanks metadata

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -2,7 +2,10 @@ import React from "react"
 import Layout from "../components/layout"
 
 export default () => (
-  <Layout>
+  <Layout
+    pageTitle="Thanks"
+    pageDescription="I appreciate your interest. Will get back to you as soon as possible."
+  >
     <h1>Thank you for getting in touch!</h1>
     <p>
       I appreciate your message. Will get back in touch with you soon.
